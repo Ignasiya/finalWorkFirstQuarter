c1b31b140c9fd8789a4c7f662435f574f31b1a64
Ignasiya <ignasiya93@gmail.com>
2023-03-20 19:23:31 +0300

    Первый вариант

--- finalWork/Program.cs
+++ finalWork/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+string[] primaryText = InputText($"Введите текст и нажмите Enter: ");
+string[] result = ResultText(primaryText, $"Введите необходимую длину символов: ");
+WriteLine($"[{String.Join(" ", result)}]");
+
+string[] InputText(string text)
+{
+    Write(text);
+    string[] arrayText = ReadLine().Split(" ");
+    return arrayText;
+}
+
+string[] ResultText(string[] array, string text)
+{
+    Write(text);
+    int size = int.Parse(Console.ReadLine());
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < size) count++;
+    }
+    string[] arrayText = new string[count];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < size)
+        {
+            arrayText[j] = array[i];
+            j++;
+        }
+    }
+    return arrayText;
+}
